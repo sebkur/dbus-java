37a361cc8e735e895ff4011304e4c10a68303e2f
Sebastian Kürten <sebastian@topobyte.de>
2017-04-02 11:42:22 +0200

    ListDisks: support -v switch via commons-cli

--- exe/src/main/java/org/freedesktop/dbus/bin/ListDisks.java
+++ exe/src/main/java/org/freedesktop/dbus/bin/ListDisks.java
@@ -2,20 +2,47 @@ package org.freedesktop.dbus.bin;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.freedesktop.DBus;
 import org.freedesktop.UDisks;
-import org.freedesktop.DBus.Properties;
 import org.freedesktop.dbus.DBusConnection;
 import org.freedesktop.dbus.Path;
 import org.freedesktop.dbus.UInt64;
+import org.freedesktop.dbus.Variant;
 import org.freedesktop.dbus.exceptions.DBusException;
+
+import de.topobyte.utilities.apache.commons.cli.OptionHelper;
 
 public class ListDisks
 {
 
+	private static final String OPTION_VERBOSE = "verbose";
+
 	public static void main(String[] args) throws DBusException
 	{
+		Options options = new Options();
+		OptionHelper.add(options, "v", OPTION_VERBOSE, false, false,
+				"show more information");
+
+		CommandLine line = null;
+		try {
+			line = new DefaultParser().parse(options, args);
+		} catch (ParseException e) {
+			System.out.println("Error while parsing command line");
+			System.out.println(e.getMessage());
+			new HelpFormatter().printHelp(ListDisks.class.getSimpleName(),
+					options);
+			System.exit(1);
+		}
+
+		boolean verbose = line.hasOption(OPTION_VERBOSE);
+
 		int connection = DBusConnection.SYSTEM;
 
 		DBusConnection conn = DBusConnection.getConnection(connection);
@@ -63,6 +90,13 @@ public class ListDisks
 			System.out.println("PresentationHide: " + presentationHide);
 			System.out.println("PartitionType: " + partitionType);
 			System.out.println("Size: " + size(size));
+
+			if (verbose) {
+				Map<String, Variant> all = diskProps.GetAll(idDevices);
+				for (String key : all.keySet()) {
+					System.out.println(key + " " + all.get(key));
+				}
+			}
 		}
 
 		conn.disconnect();
